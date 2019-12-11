28ad81a27eb97a28e3320f7a47e700335d69794d
DaMatrix <daporkchop@daporkchop.net>
2019-12-11 20:48:15 +0100

    okay i'll admit i have no idea what my plan is or what i'm doing
    i think i need to figure out how to properly initialize the console before using it, but that'll be difficult since nothing seems to make any sense

--- dsi/test/arm9/source/main.cpp
+++ dsi/test/arm9/source/main.cpp
@@ -1,6 +1,44 @@
 #include "main.h"
 
+using namespace dsi;
+
+void setBrightness( int screen, int level) {
+//---------------------------------------------------------------------------------
+    int mode = 1<<14;
+
+    if ( level < 0){
+        level = -level;
+        mode = 2<<14;
+    }
+
+    if (level>16) level =16;
+
+    if (screen & 1) reg::MASTER_BRIGHT_A(mode | level);
+    if (screen & 2) reg::MASTER_BRIGHT_B(mode | level);
+}
+
 int main() {
+    setBrightness(3, -16);
+
+    reg::POWCNT1(1);
+
+    reg::DISPCNT_A(0);
+    reg::DISPCNT_B(0);
+
+    reg::VRAMCNT_A(0);
+    reg::VRAMCNT_B(0);
+    reg::VRAMCNT_C(0);
+    reg::VRAMCNT_D(0);
+    reg::VRAMCNT_E(0);
+    reg::VRAMCNT_F(0);
+    reg::VRAMCNT_G(0);
+    reg::VRAMCNT_H(0);
+    reg::VRAMCNT_I(0);
+
+    if (true)   {
+        return 0;
+    }
+
     dsi::memory::fastCopy(nullptr, nullptr, 32);
     while (true) { dsi::bios::vBlankIntrWait(); }
     return 0;
