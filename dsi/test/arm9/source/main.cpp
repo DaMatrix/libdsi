d7947b2b0a568daf762c2bc94a44debb1e83ecbe
DaMatrix <daporkchop@daporkchop.net>
2019-12-13 21:51:09 +0100

    nothing is working...

--- dsi/test/arm9/source/main.cpp
+++ dsi/test/arm9/source/main.cpp
@@ -3,6 +3,12 @@
 using namespace dsi;
 
 int main() {
+    reg::DISPCNT_A(0x10003);
+    reg::DISPCNT_B(0x10003);
+
+    reg::BG3CNT_A(((1 << 14) | (1 << 7) | (1 << 2) | (4 << 16)));
+    reg::DISPCNT_A(reg::DISPCNT_A() | (1 << (8 + 3)));
+
     auto buf = (u16*) 0x06000000;
     u16 color = 0;
     while (true)    {
