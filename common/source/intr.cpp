97c2b5c1121596624405c7adedaec690e38cdcfe
DaMatrix <daporkchop@daporkchop.net>
2019-12-19 22:24:53 +0100

    continue implementing interrupt handler

--- common/source/intr.cpp
+++ common/source/intr.cpp
@@ -5,8 +5,25 @@ extern "C" void stdRootHandler();
 extern "C" volatile dsi::u32& __irq_flags;
 extern "C" volatile dsi::Void& __irq_vector;
 
+#ifdef ARM9
+//place interrupt handlers in dtcm on ARM9
+__attribute__((section(".dtcm")))
+#endif
+void* __interruptHandlers[32];
+
+#ifdef ARM7
+//additional interrupt handlers for ARM7
+void* __interruptHandlers7[32];
+#endif
+
 namespace dsi::intr {
     void init() {
+        //clear handlers
+        mem::fastClear(__interruptHandlers, sizeof(__interruptHandlers));
+#ifdef ARM7
+        mem::fastClear(__interruptHandlers7, sizeof(__interruptHandlers7));
+#endif
+
         setRootHandler(stdRootHandler);
     }
 
